a4028451971627cb66e07522d9721df9c71cfc1f
Jessica Wang <jessiwang2000@gmail.com>
2025-07-17 15:47:44 -0700

    remove backslashes lol

--- posthoc/nllp/finetune_ats.cmd
+++ posthoc/nllp/finetune_ats.cmd
@@ -1,9 +1,9 @@
 executable = finetune_ats.sh
 getenv = true
 transfer_executable = false
-output = finetune_ats.\$(Cluster).out
-error = finetune_ats.\$(Cluster).err
-log = finetune_ats.\$(Cluster).log
+output = finetune_ats.$(Cluster).out
+error = finetune_ats.$(Cluster).err
+log = finetune_ats.$(Cluster).log
 request_GPUs = 1
 Requirements = (Machine == "patas-gn3.ling.washington.edu")
 request_memory = 3000
