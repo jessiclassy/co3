d2e98b26c6cd83a881d23e76e82bdb395e9c647f
Jessica Wang <jessiwang2000@gmail.com>
2025-07-17 15:45:35 -0700

    Shell and Condor file

---
+++ posthoc/nllp/finetune_ats.cmd
@@ -0,0 +1,10 @@
+executable = finetune_ats.sh
+getenv = true
+transfer_executable = false
+output = finetune_ats.\$(Cluster).out
+error = finetune_ats.\$(Cluster).err
+log = finetune_ats.\$(Cluster).log
+request_GPUs = 1
+Requirements = (Machine == "patas-gn3.ling.washington.edu")
+request_memory = 3000
+queue
