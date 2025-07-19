7d93afb1453377fc9cace53dd3a422d0308d33e6
Jessica Wang <jessiwang2000@gmail.com>
2025-07-19 13:37:41 -0700

    separate log error files for both led models

--- posthoc/nllp/evaluate_ats.cmd
+++ posthoc/nllp/evaluate_ats.cmd
@@ -13,9 +13,9 @@ executable = evaluate_ats.sh
 getenv = true
 arguments = --checkpoint ./led_old_impl
 transfer_executable = false
-output = evaluate_ats.$(Cluster).out
-error = evaluate_ats.$(Cluster).err
-log = evaluate_ats.$(Cluster).log
+output = evaluate_ats_validated.$(Cluster).out
+error = evaluate_ats_validated.$(Cluster).err
+log = evaluate_ats_validated.$(Cluster).log
 request_GPUs = 1
 Requirements = (Machine == "patas-gn3.ling.washington.edu")
 request_memory = 3000
