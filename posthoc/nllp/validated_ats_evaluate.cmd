eb3e210173a44be0daafe5a023b6e94b8134fde4
Jessica Wang <jessiwang2000@gmail.com>
2025-07-19 13:32:16 -0700

    add new condor file for validated ATS model

---
+++ posthoc/nllp/validated_ats_evaluate.cmd
@@ -0,0 +1,11 @@
+executable = evaluate_ats.sh
+getenv = true
+arguments = --checkpoint ./led_old_impl
+transfer_executable = false
+output = evaluate_ats.$(Cluster).out
+error = evaluate_ats.$(Cluster).err
+log = evaluate_ats.$(Cluster).log
+request_GPUs = 1
+Requirements = (Machine == "patas-gn3.ling.washington.edu")
+request_memory = 3000
+queue
