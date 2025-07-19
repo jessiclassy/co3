0b4d7b5c5b2d04df9078284f19cb6ce6db691cb5
Jessica Wang <jessiwang2000@gmail.com>
2025-07-19 13:57:02 -0700

    use proper checkpoint paths and add process IDs to log files

--- posthoc/nllp/evaluate_ats.cmd
+++ posthoc/nllp/evaluate_ats.cmd
@@ -1,9 +1,10 @@
 executable = evaluate_ats.sh
 getenv = true
+arguments = --checkpoint ./led/checkpoint-19320/
 transfer_executable = false
-output = evaluate_ats.$(Cluster).out
-error = evaluate_ats.$(Cluster).err
-log = evaluate_ats.$(Cluster).log
+output = evaluate_ats.$(Cluster).$(Process).out
+error = evaluate_ats.$(Cluster).$(Process).err
+log = evaluate_ats.$(Cluster).$(Process).log
 request_GPUs = 1
 Requirements = (Machine == "patas-gn3.ling.washington.edu")
 request_memory = 3000
@@ -11,11 +12,35 @@ queue
 
 executable = evaluate_ats.sh
 getenv = true
-arguments = --checkpoint ./led_old_impl/
+arguments = --checkpoint ./led/checkpoint-19322/
 transfer_executable = false
-output = evaluate_ats_validated.$(Cluster).out
-error = evaluate_ats_validated.$(Cluster).err
-log = evaluate_ats_validated.$(Cluster).log
+output = evaluate_ats.$(Cluster).$(Process).out
+error = evaluate_ats.$(Cluster).$(Process).err
+log = evaluate_ats.$(Cluster).$(Process).log
+request_GPUs = 1
+Requirements = (Machine == "patas-gn3.ling.washington.edu")
+request_memory = 3000
+queue
+
+executable = evaluate_ats.sh
+getenv = true
+arguments = --checkpoint ./led_old_impl/checkpoint-19320/
+transfer_executable = false
+output = evaluate_ats_validated.$(Cluster).$(Process).out
+error = evaluate_ats_validated.$(Cluster).$(Process).err
+log = evaluate_ats_validated.$(Cluster).$(Process).log
+request_GPUs = 1
+Requirements = (Machine == "patas-gn3.ling.washington.edu")
+request_memory = 3000
+queue
+
+executable = evaluate_ats.sh
+getenv = true
+arguments = --checkpoint ./led_old_impl/checkpoint-19322/
+transfer_executable = false
+output = evaluate_ats_validated.$(Cluster).$(Process).out
+error = evaluate_ats_validated.$(Cluster).$(Process).err
+log = evaluate_ats_validated.$(Cluster).$(Process).log
 request_GPUs = 1
 Requirements = (Machine == "patas-gn3.ling.washington.edu")
 request_memory = 3000
