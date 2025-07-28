52d851b09fd8d2a8c90e8410f1ead8258c1fdfed
Jessica Wang <jessiwang2000@gmail.com>
2025-07-28 00:42:06 -0700

    add env variables for logging cluster and process on sibling jobs

--- posthoc/nllp/generate_ats.cmd
+++ posthoc/nllp/generate_ats.cmd
@@ -1,10 +1,24 @@
 executable = generate_ats.sh
 getenv = true
-arguments = --checkpoint ./led_old_impl/checkpoint-19322/ --output_dir ats_2048_max_input
+environment = "CLUSTER_ID=$(Cluster) PROCESS_ID=$(Process)"
+arguments = --checkpoint ./led_old_impl/checkpoint-19322/ --output_dir 2048_max_input
 transfer_executable = false
-output = generate_ats_validated.$(Cluster).$(Process).out
-error = generate_ats_validated.$(Cluster).$(Process).err
-log = generate_ats_validated.$(Cluster).$(Process).log
+output = generate_ats_2048.out
+error = generate_ats_2048.err
+log = generate_ats_2048.log
+request_GPUs = 1
+Requirements = (Machine == "patas-gn3.ling.washington.edu")
+request_memory = 1600
+queue
+
+executable = generate_ats.sh
+getenv = true
+environment = "CLUSTER_ID=$(Cluster) PROCESS_ID=$(Process)"
+arguments = --checkpoint ./led_1024/checkpoint-19322/ --output_dir 1024_max_input
+transfer_executable = false
+output = generate_ats_1024.out
+error = generate_ats_1024.err
+log = generate_ats_1024.log
 request_GPUs = 1
 Requirements = (Machine == "patas-gn3.ling.washington.edu")
 request_memory = 1600
--- posthoc/nllp/minwiki.cmd
+++ posthoc/nllp/minwiki.cmd
@@ -1,10 +1,11 @@
 executable = minwiki.sh
 getenv = true
+environment = "CLUSTER_ID=$(Cluster) PROCESS_ID=$(Process)"
 arguments = --checkpoint ./led_1024/checkpoint-19322/
 transfer_executable = false
-output = evaluate_1024.$(Cluster).$(Process).out
-error = evaluate_1024.$(Cluster).$(Process).err
-log = evaluate_1024.$(Cluster).$(Process).log
+output = evaluate_1024.out
+error = evaluate_1024.err
+log = evaluate_1024.log
 request_GPUs = 1
 Requirements = (Machine == "patas-gn3.ling.washington.edu")
 request_memory = 1500
