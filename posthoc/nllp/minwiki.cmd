32713347916f525f8372fa195a3dd48f17fc7cff
Jessica Wang <jessiwang2000@gmail.com>
2025-07-28 00:22:13 -0700

    change evaluate ATS condor file for LED 1024

--- posthoc/nllp/minwiki.cmd
+++ posthoc/nllp/minwiki.cmd
@@ -1,46 +1,10 @@
 executable = minwiki.sh
 getenv = true
-arguments = --checkpoint ./led/checkpoint-19320/
+arguments = --checkpoint ./led_1024/checkpoint-19322/
 transfer_executable = false
-output = evaluate_minwiki.$(Cluster).$(Process).out
-error = evaluate_minwiki.$(Cluster).$(Process).err
-log = evaluate_minwiki.$(Cluster).$(Process).log
-request_GPUs = 1
-Requirements = (Machine == "patas-gn3.ling.washington.edu")
-request_memory = 1500
-queue
-
-executable = minwiki.sh
-getenv = true
-arguments = --checkpoint ./led/checkpoint-19322/
-transfer_executable = false
-output = evaluate_minwiki.$(Cluster).$(Process).out
-error = evaluate_minwiki.$(Cluster).$(Process).err
-log = evaluate_minwiki.$(Cluster).$(Process).log
-request_GPUs = 1
-Requirements = (Machine == "patas-gn3.ling.washington.edu")
-request_memory = 1500
-queue
-
-executable = minwiki.sh
-getenv = true
-arguments = --checkpoint ./led_old_impl/checkpoint-19320/
-transfer_executable = false
-output = evaluate_minwiki_validated.$(Cluster).$(Process).out
-error = evaluate_minwiki_validated.$(Cluster).$(Process).err
-log = evaluate_minwiki_validated.$(Cluster).$(Process).log
-request_GPUs = 1
-Requirements = (Machine == "patas-gn3.ling.washington.edu")
-request_memory = 1500
-queue
-
-executable = minwiki.sh
-getenv = true
-arguments = --checkpoint ./led_old_impl/checkpoint-19322/
-transfer_executable = false
-output = evaluate_minwiki_validated.$(Cluster).$(Process).out
-error = evaluate_minwiki_validated.$(Cluster).$(Process).err
-log = evaluate_minwiki_validated.$(Cluster).$(Process).log
+output = evaluate_1024.$(Cluster).$(Process).out
+error = evaluate_1024.$(Cluster).$(Process).err
+log = evaluate_1024.$(Cluster).$(Process).log
 request_GPUs = 1
 Requirements = (Machine == "patas-gn3.ling.washington.edu")
 request_memory = 1500
