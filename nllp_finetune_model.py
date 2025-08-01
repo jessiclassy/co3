9f7afe67698591ef31f201b88d820d90f2d073b5
Jessica Wang <jessiwang2000@gmail.com>
2025-08-01 11:50:55 -0700

    handoff - working on final data update function now

--- nllp_finetune_model.py
+++ nllp_finetune_model.py
@@ -5,7 +5,7 @@ import torch
 from datasets import Dataset, load_dataset
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, Seq2SeqTrainingArguments, Seq2SeqTrainer
 from transformers import set_seed
-from utils import *
+import utils
 import re
 import sys
 
@@ -31,8 +31,8 @@ def finetune(
         grad_acc: adaptive gradient hyperparameter
         num_epochs: number of training rounds/batches to finetune on
         random_seed: random seed for replicability
-        train_data: the training data as a hugging face Dataset object
-        dev_data: development data partition for selecting the best model during training
+        train_data: the training data as a HuggingFace Dataset object
+        dev_data: development data partition (HuggingFace Dataset) for selecting the best model during training
     Returns:
         None
     """
@@ -63,30 +63,52 @@ def finetune(
     return
 
 def update_data(
-        train_data: pd.DataFrame, 
-        blank_target_setting: str, 
-        control_token_id: int
+        train_data: pd.DataFrame,
+        updated_tokenizer: AutoTokenizer, 
+        blank_target_setting: str,
+        random_seed: int,
+        max_input_length: int,
+        max_output_length: int
     ):
     """Takes a pandas dataframe object, and converts it to a huggingface Dataset object.
     Modifies empty targets to be the custom token. Handles exclusion of the custom token.
 
     Arguments:
         train_data: The training data as a pandas.DataFrame
+        updated_tokenizer: AutoTokenizer object (with special token added) for final data processing step
         blank_target_setting: "keep" or "drop" for inclusion/exclusion of the custom token
-        control_token_id: The integer value of the custom token
-    
+        random_seed: Random seed value to ensure consistent train/dev partitioning
     Returns:
         the training data and development data as HF Dataset
     """
-    # TODO: use train_test_split function from datasets
+    # Fill null summary examples according to blank_target_setting
+    if blank_target_setting == "keep":
+        # Filter the training data by any null document chunks (if any) as a precaution
+        train_data = train_data.loc[train_data.text.notna()]
+        # Add the special token string for null summary targets
+        train_data.loc[train_data.summary.isna(), 'summary'] = "[NO_SUMMARY]"
+    else:
+        train_data = train_data.dropna() # Drop any row with ANY null values
+
+    # convert to HF Dataset type
+    data_hf = Dataset.from_pandas(train_data)
+
+    # tokenize data
+    prepare_examples = utils.create_examples(
+        max_input_len=max_input_length, 
+        max_output_len=max_output_length,
+        tokenizer=updated_tokenizer
+        )
+    
+    examples = data_hf.map(prepare_examples, batched=True)
+    # TODO: use train_test_split function to create a development partition
     train_hf = None
     dev_hf = None
     return train_hf, dev_hf
 
 def update_model_tokenizer(
         model: AutoModelForSeq2SeqLM,
-        tokenizer: AutoTokenizer,
-        blank_target_setting: str # wondering if we need this arg here?
+        tokenizer: AutoTokenizer
     ):
     """Modifies the model vocabulary to include the custom 
     [NO_SUMMARY] token
@@ -97,8 +119,17 @@ def update_model_tokenizer(
         blank_target_setting: "keep" or "drop" for inclusion/exclusion in training data
 
     Returns:
-        the model and tokenizer
+        the model, tokenizer
     """
+    special_tokens_dict = {"no_summary_token": "[NO_SUMMARY]"}
+
+    num_added_toks = tokenizer.add_special_tokens(special_tokens_dict)
+    print("We have added", num_added_toks, "special tokens")
+
+    # Notice: resize_token_embeddings expect to receive the full size of the new vocabulary, i.e., the length of the tokenizer.
+    model.resize_token_embeddings(len(tokenizer))
+
+    assert tokenizer.no_summary_token == "[NO_SUMMARY]"
     return model, tokenizer
 
 def load_data(sourcefile: str):
@@ -196,20 +227,26 @@ def prepare_output_dirs(
 
 def load_model_tokenizer(
         checkpoint: str, 
-        device: torch.device, 
         max_output_length: int
     ): # could this function include the device assignment ~line 251
     """Loads the model tokenizer and handles torch device assignment
 
     Arguments:
         checkpoint: name of HuggingFace model
-        device: specified device to allocate Tensors
         max_output_length: maximum number of output tokens
     Returns:
         model_name: simplified model name as a string for output directory
         model: AutoModelForSeq2SeqLM object
         tokenizer: AutoTokenizer object
+        device: torch.device object for saving Tensors
+        has_global_attn: boolean for data preprocessing
     """
+    # Set device
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # Argument for Longformer usage
+    has_global_attn = "allenai/led" in checkpoint
+
     # Model name and settings
     model_name = '-'.join(checkpoint.split("/")[1].split("-")[0:2])
     model = AutoModelForSeq2SeqLM.from_pretrained(checkpoint).to(device)
@@ -221,7 +258,7 @@ def load_model_tokenizer(
 
     # Instantiate tokenizer
     tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-    return model_name, model, tokenizer
+    return model_name, model, tokenizer, device, has_global_attn
 
 def load_args():
     parser = argparse.ArgumentParser()
@@ -245,10 +282,6 @@ def main():
     ############ INFERRED ARGUMENTS ###########
     # Set random seed
     set_seed(args.seed)
-
-    # Argument for Longformer usage
-    has_global_attn = args.checkpoint == "allenai/led-base-16384"
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     ############################################
 
@@ -276,9 +309,8 @@ def main():
         max_output_len = train_max_output_len
 
     # load model, tokenizer 
-    model_name, model, tokenizer = load_model_tokenizer(
+    model_name, model, tokenizer, device, has_global_attn = load_model_tokenizer(
         checkpoint=args.checkpoint,
-        device=device,
         max_output_length=max_output_len
     )
 
@@ -300,7 +332,7 @@ def main():
     train_hf, dev_hf = update_data(
         train_data=train_data,
         blank_target_setting=args.blank_targets,
-        control_token_id=None
+        updated_tokenizer=tokenizer
     )
 
     # train model
