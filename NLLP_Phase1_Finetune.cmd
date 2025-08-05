f1dae54afb172e562312de97eb90268902b5476a
Jessica Wang <jessiwang2000@gmail.com>
2025-08-05 13:02:35 -0700

    add env vars, update condor output files

--- NLLP_Phase1_Finetune.cmd
+++ NLLP_Phase1_Finetune.cmd
@@ -3,11 +3,12 @@
 ############
 executable = nllp_finetune_model.sh
 getenv = true
+environment = "CLUSTER_ID=$(Cluster); PROCESS_ID=$(Process);"
 arguments = --checkpoint allenai/led-base-16384 --mode train --trainfile preprocess/nllp_data/billsum_clean_train_se3-led-1024-512_simple.csv --batch_size 4 --blank_targets drop
 transfer_executable = false
-output = nllp_finetune_model.$(Cluster).out
-error = nllp_finetune_model.$(Cluster).err
-log = nllp_finetune_model.$(Cluster).log
+output = phase_1.$(Cluster).$(Process).out
+error = phase_1.$(Cluster).$(Process).err
+log = phase_1.$(Cluster).$(Process).log
 request_GPUs = 1
 Requirements = (Machine == "patas-gn3.ling.washington.edu")
 request_memory = 3000
@@ -19,11 +20,12 @@ queue
 ############
 executable = nllp_finetune_model.sh
 getenv = true
+environment = "CLUSTER_ID=$(Cluster); PROCESS_ID=$(Process);"
 arguments = --checkpoint allenai/led-base-16384 --mode train --trainfile preprocess/nllp_data/billsum_clean_train_se3-led-2048-512_simple.csv --batch_size 4 --blank_targets drop
 transfer_executable = false
-output = nllp_finetune_model.$(Cluster).out
-error = nllp_finetune_model.$(Cluster).err
-log = nllp_finetune_model.$(Cluster).log
+output = phase_1.$(Cluster).$(Process).out
+error = phase_1.$(Cluster).$(Process).err
+log = phase_1.$(Cluster).$(Process).log
 request_GPUs = 1
 Requirements = (Machine == "patas-gn3.ling.washington.edu")
 request_memory = 3000
@@ -35,11 +37,12 @@ queue
 ############
 executable = nllp_finetune_model.sh
 getenv = true
+environment = "CLUSTER_ID=$(Cluster); PROCESS_ID=$(Process);"
 arguments = --checkpoint allenai/led-base-16384 --mode train --trainfile preprocess/nllp_data/billsum_clean_train_se3-led-1024-512.csv --batch_size 4 --blank_targets drop
 transfer_executable = false
-output = nllp_finetune_model.$(Cluster).out
-error = nllp_finetune_model.$(Cluster).err
-log = nllp_finetune_model.$(Cluster).log
+output = phase_1.$(Cluster).$(Process).out
+error = phase_1.$(Cluster).$(Process).err
+log = phase_1.$(Cluster).$(Process).log
 request_GPUs = 1
 Requirements = (Machine == "patas-gn3.ling.washington.edu")
 request_memory = 3000
@@ -51,11 +54,12 @@ queue
 ############
 executable = nllp_finetune_model.sh
 getenv = true
+environment = "CLUSTER_ID=$(Cluster); PROCESS_ID=$(Process);"
 arguments = --checkpoint allenai/led-base-16384 --mode train --trainfile preprocess/nllp_data/billsum_clean_train_se3-led-2048-512.csv --batch_size 4 --blank_targets drop
 transfer_executable = false
-output = nllp_finetune_model.$(Cluster).out
-error = nllp_finetune_model.$(Cluster).err
-log = nllp_finetune_model.$(Cluster).log
+output = phase_1.$(Cluster).$(Process).out
+error = phase_1.$(Cluster).$(Process).err
+log = phase_1.$(Cluster).$(Process).log
 request_GPUs = 1
 Requirements = (Machine == "patas-gn3.ling.washington.edu")
 request_memory = 3000
