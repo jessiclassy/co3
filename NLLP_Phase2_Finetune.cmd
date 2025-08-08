d246d7b24fc7a3f6d6a715ceb912a28e9963293d
Jessica Wang <jessiwang2000@gmail.com>
2025-08-07 22:04:48 -0700

    clean Phase 2 batch Condor job

--- NLLP_Phase2_Finetune.cmd
+++ NLLP_Phase2_Finetune.cmd
@@ -1,10 +1,27 @@
 ############
-# LED-base, ?????? input (??????ATS)
+# LED-base, 1024 input (-ATS)
 ############
 executable = nllp_finetune_model.sh
 getenv = true
 environment = "CLUSTER_ID=$(Cluster); PROCESS_ID=$(Process);"
-arguments = --checkpoint allenai/led-base-16384 --mode train --trainfile preprocess/nllp_data/??????? --batch_size 4 --blank_targets ?????????
+arguments = --checkpoint allenai/led-base-16384 --mode train --trainfile preprocess/nllp_databillsum_clean_train_se3-led-1024-512.csv --batch_size 4 --blank_targets keep
+transfer_executable = false
+output = phase_1.$(Cluster).$(Process).out
+error = phase_1.$(Cluster).$(Process).err
+log = phase_1.$(Cluster).$(Process).log
+request_GPUs = 1
+Requirements = (Machine == "patas-gn3.ling.washington.edu")
+request_memory = 3000
+notification = error
+queue
+
+############
+# LED-base, 2048 input (+ATS)
+############
+executable = nllp_finetune_model.sh
+getenv = true
+environment = "CLUSTER_ID=$(Cluster); PROCESS_ID=$(Process);"
+arguments = --checkpoint allenai/led-base-16384 --mode train --trainfile preprocess/nllp_databillsum_clean_train_se3-led-2048-512_simple.csv --batch_size 4 --blank_targets keep
 transfer_executable = false
 output = phase_1.$(Cluster).$(Process).out
 error = phase_1.$(Cluster).$(Process).err
