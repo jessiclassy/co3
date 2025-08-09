173194d9521f4f12ca7d14f0ed45d6f70daafe95
Jessica Wang <jessiwang2000@gmail.com>
2025-08-08 18:28:09 -0700

    fix log files too oops

--- NLLP_Phase2_Finetune.cmd
+++ NLLP_Phase2_Finetune.cmd
@@ -6,9 +6,9 @@ getenv = true
 environment = "CLUSTER_ID=$(Cluster); PROCESS_ID=$(Process);"
 arguments = --checkpoint allenai/led-base-16384 --mode train --trainfile preprocess/nllp_databillsum_clean_train_se3-led-1024-512.csv --batch_size 4 --blank_targets keep
 transfer_executable = false
-output = phase_1.$(Cluster).$(Process).out
-error = phase_1.$(Cluster).$(Process).err
-log = phase_1.$(Cluster).$(Process).log
+output = phase_2.$(Cluster).$(Process).out
+error = phase_2.$(Cluster).$(Process).err
+log = phase_2.$(Cluster).$(Process).log
 request_GPUs = 1
 Requirements = (Machine == "patas-gn3.ling.washington.edu")
 request_memory = 3000
@@ -23,9 +23,9 @@ getenv = true
 environment = "CLUSTER_ID=$(Cluster); PROCESS_ID=$(Process);"
 arguments = --checkpoint allenai/led-base-16384 --mode train --trainfile preprocess/nllp_databillsum_clean_train_se3-led-2048-512_simple.csv --batch_size 4 --blank_targets keep
 transfer_executable = false
-output = phase_1.$(Cluster).$(Process).out
-error = phase_1.$(Cluster).$(Process).err
-log = phase_1.$(Cluster).$(Process).log
+output = phase_2.$(Cluster).$(Process).out
+error = phase_2.$(Cluster).$(Process).err
+log = phase_2.$(Cluster).$(Process).log
 request_GPUs = 1
 Requirements = (Machine == "patas-gn3.ling.washington.edu")
 request_memory = 3000
