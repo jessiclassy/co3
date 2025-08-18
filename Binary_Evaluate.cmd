399f9216fec8dfad201d7ba89c41abe659f565a5
Jessica Wang <jessiwang2000@gmail.com>
2025-08-17 21:20:28 -0700

    Condor jobs for final experiment sets

--- Binary_Evaluate.cmd
+++ Binary_Evaluate.cmd
@@ -1,4 +1,25 @@
 ############
+# LED-base, 2048 input (-ATS, + binary blanks)
+############
+executable = nllp_evaluate_model.sh
+getenv = true
+environment = "CLUSTER_ID=$(Cluster); PROCESS_ID=$(Process);"
+arguments = --config_id 4 --base_tokenizer allenai/led-base-16384 \
+--checkpoint /home2/jcmw614/ling573/models/led-base/billsum_clean_train_se3-led-2048-512/binary_blank_targets/2048_512_5_epochs/checkpoint-108471/ \
+--mode test \
+--testfile preprocess/nllp_data/billsum_clean_test_se3-led-2048-512.csv \
+--batch_size 6 
+transfer_executable = false
+output = evaluate_phase_2.$(Cluster).$(Process).out
+error = evaluate_phase_2.$(Cluster).$(Process).err
+log = evaluate_phase_2.$(Cluster).$(Process).log
+request_GPUs = 1
+Requirements = (Machine == "patas-gn3.ling.washington.edu")
+request_memory = 6000
+notification = error
+queue
+
+############
 # LED-base, 1024 input (-ATS, + binary blanks)
 ############
 executable = nllp_evaluate_model.sh
--- posthoc/probe.cmd
+++ posthoc/probe.cmd
@@ -1,5 +1,5 @@
 ############
-# LED-base, 2048 input (+ATS, +blanks)
+# LED-base, 1024 input (-ATS, +blanks)
 ############
 executable = posthoc/probe.sh
 getenv = true
@@ -20,5 +20,22 @@ notification = error
 queue
 
 ############
-# LED-base, 1024 input (-ATS, +blanks)
+# LED-base, 2048 input (-ATS, +blanks)
 ############
+executable = posthoc/probe.sh
+getenv = true
+environment = "CLUSTER_ID=$(Cluster); PROCESS_ID=$(Process);"
+arguments = --config_id 5 --base_tokenizer allenai/led-base-16384 \
+--checkpoint /home2/jcmw614/ling573/models/led-base/billsum_clean_train_se3-led-2048-512/binary_blank_targets/2048_512_5_epochs/checkpoint-108471/ \
+--mode test \
+--testfile preprocess/nllp_data/billsum_clean_test_se3-led-2048-512.csv \
+--batch_size 6 
+transfer_executable = false
+output = probe_binary_phase_2.$(Cluster).$(Process).out
+error = probe_binary_phase_2.$(Cluster).$(Process).err
+log = probe_binary_phase_2.$(Cluster).$(Process).log
+request_GPUs = 1
+Requirements = (Machine == "patas-gn3.ling.washington.edu")
+request_memory = 5400
+notification = error
+queue
