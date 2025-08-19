8ebc9cb16a9c57d89152c3b7c1b6870d90dea5b9
Jessica Wang <jessiwang2000@gmail.com>
2025-08-18 20:42:06 -0700

    update filepath in probe cmd

--- posthoc/probe.cmd
+++ posthoc/probe.cmd
@@ -26,7 +26,7 @@ executable = posthoc/probe.sh
 getenv = true
 environment = "CLUSTER_ID=$(Cluster); PROCESS_ID=$(Process);"
 arguments = --config_id 4 --base_tokenizer allenai/led-base-16384 \
---checkpoint /home2/jcmw614/ling573/models/led-base/billsum_clean_train_se3-led-1024-512/binary_blank_targets/1024_512_5_epochs/checkpoint-72314/ \
+--checkpoint /home2/jcmw614/ling573/models/led-base/billsum_clean_train_se3-led-1024-512/binary_blank_targets/1024_512_5_epochs/checkpoint-108471/ \
 --mode test \
 --testfile preprocess/nllp_data/billsum_clean_test_se3-led-1024-512.csv \
 --batch_size 6 
